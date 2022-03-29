b9f060e3b948d8346d7d9a334dfd157e4eda4e5a
Shabeeb K <mail@shabeebk.com>
2022-03-29 15:07:50 +0530

    simple TODO app

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TodoList />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from "./components/TodoList.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TodoList,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,157 @@
+<template>
+  <div class="hello">
+    <section class="vh-100" style="background-color: #eee">
+      <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col col-lg-9 col-xl-7">
+            <div class="card rounded-3">
+              <div class="card-body p-4">
+                <h4 class="text-center my-3 pb-3">Simple Vue 2 To Do App</h4>
+
+                <form
+                  class="row row-cols-lg-auto g-3 justify-content-center align-items-center mb-4 pb-2"
+                  @submit.prevent="addToDo"
+                >
+                  <div class="col-12">
+                    <div class="form-outline">
+                      <input
+                        type="text"
+                        id="form1"
+                        v-model="newTodo"
+                        class="form-control"
+                      />
+                      <label class="form-label" for="form1"
+                        >Enter a task here</label
+                      >
+                    </div>
+                  </div>
+
+                  <div class="col-12">
+                    <button
+                      type="button"
+                      class="btn btn-primary"
+                      @click="addToDo"
+                    >
+                      Save
+                    </button>
+                  </div>
+
+                  <div class="col-12">
+                    <button type="submit" class="btn btn-warning">
+                      Get tasks
+                    </button>
+                  </div>
+                </form>
+
+                <table class="table mb-4">
+                  <thead>
+                    <tr>
+                      <th scope="col">No.</th>
+                      <th scope="col">Todo item</th>
+                      <th scope="col">Status</th>
+                      <th scope="col">Actions</th>
+                    </tr>
+                  </thead>
+
+                  <tbody>
+                    <tr
+                      v-for="(todo, idx) in todos"
+                      :key="todo.id"
+                      :class="todo.completed && 'stricked'"
+                    >
+                      <th scope="row">{{ idx + 1 }}</th>
+                      <td>{{ todo.title }}</td>
+                      <td>
+                        {{ todo.completed ? "Completed" : "In progress" }}
+                      </td>
+                      <td>
+                        <button
+                          type="submit"
+                          class="btn btn-danger"
+                          @click="removeItem(todo)"
+                        >
+                          Delete
+                        </button>
+                        <button
+                          type="submit"
+                          class="btn btn-success ms-1"
+                          @click="toggleCompleted(todo)"
+                        >
+                          {{ todo.completed ? "Re-open" : "Complete" }}
+                        </button>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      newTodo: "",
+      todos: [
+        {
+          id: 1,
+          title: "Cook Food ",
+          completed: false,
+        },
+        {
+          id: 2,
+          title: "Clean ",
+          completed: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    addToDo(e) {
+      e.preventDefault();
+      this.todos.push({
+        id: this.todos.length + 1,
+        title: this.newTodo,
+        completed: false,
+      });
+      this.newTodo = "";
+    },
+    toggleCompleted: function (item) {
+      item.completed = !item.completed;
+    },
+    removeItem: function (item) {
+      this.todos = this.todos.filter((newItem) => newItem.id !== item.id);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #37dd92;
+}
+.stricked {
+  text-decoration: line-through;
+}
+</style>
